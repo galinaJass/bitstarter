f048fe3bd5fcc32bf61adcda239d2be2e2c6114e
Ubuntu <ubuntu@ip-172-31-41-119.us-west-2.compute.internal>
2013-07-30 20:49:12 +0000

    nothing changed test for quiz

--- web.js
+++ web.js
@@ -14,6 +14,7 @@ app.get('/', function(request, response){
     response.send(buf.toString());
 });
 
+//to see in a browser don't stop the process "node web.js"
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
